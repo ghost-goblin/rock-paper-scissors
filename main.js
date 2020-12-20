b7ac9af99e1e6f9ea0377ae39413a573eb1a10a0
Chloe Urisohn <chloeurisohn@gmail.com>
2020-12-20 12:32:11 +0000

    first commit

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3 ) + 1;
     if (computerChoice == 1){
-        return "Rock✊";
+        return "rock";
     } else if (computerChoice == 2) {
-        return "Paper✋";
-    } else return "Scissors✌️";
+        return "paper";
+    } else return "scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection == "Rock✊" && playerSelection == "Rock") {
-        return `The computer chose ${computerSelection} and you chose ${playerSelection}! We have a draw!`;
-    } else if (computerSelection == "Paper✋" && playerSelection == "Rock") {
+    if (computerSelection == "rock" && playerSelection == "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`; 
+    } else if (computerSelection == "paper" && playerSelection == "scissors") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }  else if (computerSelection == "paper" && playerSelection == "rock") {
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    } else if (computerSelection == "Scissors✌️" && playerSelection == "Rock") {
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`; 
+    } else if (computerSelection == "scissors" && playerSelection == "rock") {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
+    else return `The computer chose ${computerSelection} and you chose ${playerSelection}! We have a draw!`;
   }
 
 const computerSelection = computerPlay();
-const playerSelection = "Rock";
+const playerSelection = "paper";
 console.log(playRound(playerSelection, computerSelection));
+
+console.log(`The computer chose ${computerPlay()}`);
+console.log(`The computer chose ${playerSelection}`);
